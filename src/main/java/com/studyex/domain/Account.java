18d1032b0a9a07e2e2a7f9b8b6a43ad4bd88d779
dmcho <dm.cho91@gmail.com>
2021-12-01 11:30:05 +0900

    add Account domain class

---
+++ src/main/java/com/studyex/domain/Account.java
@@ -0,0 +1,47 @@
+package com.studyex.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String mail;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio; // 자기소개
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+    private boolean studyCreatedByWeb;
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+    private boolean studyUpdateByEmail;
+    private boolean studyUpdateByWeb;
+}
